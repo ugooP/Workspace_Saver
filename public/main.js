d67c48a74eb59aa2318d7935c100e2af01560642
Ugo <ugo.prenat@gmail.com>
2021-01-02 09:35:48 +0100

    Détection des applications du système + lancement d'application + Electron initialisé

---
+++ public/main.js
@@ -0,0 +1,58 @@
+let input = document.querySelector('input')
+let ul = document.querySelector('ul')
+let btn = document.querySelector('button')
+let appList
+
+fetch('/api/appList', { method: 'GET' })
+    .then((response) => response.json())
+    .then((data) => {
+        appList = data
+
+        // Display all programs
+        for (let i = 0; i < data.length; i++) {
+            const app = data[i].name;
+            ul.insertAdjacentHTML('beforeend', `
+                <li onclick="fillInput('${app}')">${app}</li>
+            `)
+        }
+    })
+
+// Search system
+input.addEventListener('input', () => {
+    let listLi = document.querySelectorAll('li')
+    userInput = input.value
+    
+    for (let i = 0; i < appList.length; i++) {
+        const app = appList[i];
+        if (app.name.toLowerCase().substring(0, userInput.length) === userInput.toLowerCase()) {
+            listLi[i].style.display = ''
+        } else {
+            listLi[i].style.display = 'none'
+        }
+    }
+})
+
+// Run the selected app
+btn.addEventListener('click', () => {
+    let path
+    // Get the path of the selected prorgam
+    for (let i = 0; i < appList.length; i++) {
+        if (input.value.toLowerCase() === appList[i].name.toLowerCase()) {
+            path = appList[i].path;
+            break
+        }
+    }
+
+    let options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'text/plain',
+        },
+        body: path
+    }
+    fetch('/api/appList', options)
+})
+    
+function fillInput(app) {
+    input.value = app
+}
---
+++ server.js
@@ -0,0 +1,52 @@
+const exec = require('child_process').exec
+const open = require('open')
+
+// Server initiation
+const express = require('express')
+const app = express()
+app.listen(3000, () => { console.log('Server started') })
+app.use(express.static('public'))
+app.use(express.text())
+
+// Get all applications of the user
+let bigAppList = []
+searchForApp('Applications')
+searchForApp('System/Applications')
+searchForApp('System/Applications/Utilities')
+
+function searchForApp(directory) {
+    exec('ls', { cwd: `/${directory}`}, (error, data, getter) => {
+        let appList = data.split('\n')
+    
+        for (let i = 0; i < appList.length; i++) {
+            const app = appList[i];
+            let regExp = new RegExp('\.app$')
+    
+            if (regExp.test(app)) {
+                createObject(`${directory}/${app}`)
+            }
+        }
+    })
+}
+
+function createObject(fullAppPath) {
+    
+    let newPath = fullAppPath.replace('.app', '')
+    let newName = newPath.split('/')
+    let name = newName[newName.length - 1]
+
+    let obj = {
+        "name": name,
+        "path": fullAppPath
+    }
+
+    bigAppList.push(obj)
+}
+
+app.get('/api/appList', (req, res) => {
+    res.send(JSON.stringify(bigAppList))
+})
+
+app.post('/api/appList', (req, res) => {
+    open(`/${req.body}`)
+})
