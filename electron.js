9d34779013477a8a70bd7737b3000de5a9602d4d
Ugo <ugo.prenat@gmail.com>
2021-01-04 18:06:21 +0100

    initialisation du serveur au lancement du programme avec Electron ok

--- electron.js
+++ electron.js
@@ -1,27 +1,85 @@
 const { app, BrowserWindow } = require('electron')
 
 function createWindow () {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  })
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
 
-  win.loadFile('public/index.html')
+    startServer()
+    win.loadURL('http://localhost:3000/')
 }
+
+function startServer() {
+    const exec = require('child_process').exec
+    const open = require('open')
+    
+    // Server initiation
+    const express = require('express')
+    const app = express()
+    app.listen(3000, () => { console.log('Server started') })
+    
+    app.use(express.static('public'))
+    app.use(express.text())
+    
+    // Get all applications of the user
+    let bigAppList = []
+    searchForApp('Applications')
+    searchForApp('System/Applications')
+    searchForApp('System/Applications/Utilities')
+    
+    function searchForApp(directory) {
+        exec('ls', { cwd: `/${directory}`}, (error, data, getter) => {
+            let appList = data.split('\n')
+        
+            for (let i = 0; i < appList.length; i++) {
+                const app = appList[i];
+                let regExp = new RegExp('\.app$')
+        
+                if (regExp.test(app)) {
+                    createObject(`${directory}/${app}`)
+                }
+            }
+        })
+    }
+    
+    function createObject(fullAppPath) {
+        
+        let newPath = fullAppPath.replace('.app', '')
+        let newName = newPath.split('/')
+        let name = newName[newName.length - 1]
+    
+        let obj = {
+            "name": name,
+            "path": fullAppPath
+        }
+    
+        bigAppList.push(obj)
+    }
+    
+    app.get('/api/appList', (req, res) => {
+        res.send(JSON.stringify(bigAppList))
+    })
+    
+    app.post('/api/appList', (req, res) => {
+        open(`/${req.body}`)
+    })
+}
+
 
 app.whenReady().then(createWindow)
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
 
 app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
-  }
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
 })
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const open = require('open')
 const express = require('express')
 const app = express()
 app.listen(3000, () => { console.log('Server started') })
+
 app.use(express.static('public'))
 app.use(express.text())
 
